2d57996cb9066d207cf24d039c4531757363d3be
Amlan <amlana.dwivedy@ymedialabs.com>
2014-12-22 16:25:59 +0530

    Arduino Code Added

---
+++ Arduino/test.ino
@@ -0,0 +1,27 @@
+char incomingByte;  // incoming data
+long  FAN_LED = 12;      // LED pin for fan
+long  HEATER_LED = 2;
+ 
+void setup() {
+  Serial.begin(9600); // initialization
+  pinMode(FAN_LED, OUTPUT);
+  pinMode(HEATER_LED, OUTPUT);
+  Serial.println("Press 1 to LED ON or 0 to LED OFF...");
+}
+ 
+void loop() {
+  if (Serial.available() > 0) {  // if the data came
+    incomingByte = Serial.read(); // read byte
+    Serial.println(incomingByte);
+    if(incomingByte == '0') {
+       digitalWrite(FAN_LED, LOW);  // if 1, switch LED Off
+       digitalWrite(HEATER_LED, HIGH);  // if 1, switch LED Off
+       Serial.println("LED OFF. Press 1 to LED ON!");  // print message
+    }
+    if(incomingByte == '1') {
+       digitalWrite(FAN_LED, HIGH); // if 0, switch LED on
+       digitalWrite(HEATER_LED, LOW);
+       Serial.println("LED ON. Press 0 to LED OFF!");
+    }
+  }
+}
